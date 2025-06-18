a305465585d409cd97e30e4c305bf7e936ccb45d
Luong Minh Nhat <nhatluong.it@gmail.com>
2025-06-18 13:48:17 +0700

    Update Get Request By Status

--- back-end/routes/expenseRequest.route.js
+++ back-end/routes/expenseRequest.route.js
@@ -1,24 +1,26 @@
 import { Router } from "express";
-import { 
-    createExpenseRequestController,
-    getPendingRequestsController, 
-    getRequestByIdController, 
-    approveRequestController, 
-    rejectRequestController,
-    getEmployeeRequestsController,
-    updateExpenseRequestController,
-    deleteExpenseRequestController
+import {
+  createExpenseRequestController,
+  getPendingRequestsController,
+  getRequestByIdController,
+  approveRequestController,
+  rejectRequestController,
+  getEmployeeRequestsController,
+  updateExpenseRequestController,
+  deleteExpenseRequestController,
+  getRequestsByStatusController,
 } from "../controllers/expenseRequest.controller.js";
 
 const router = Router();
 
-router.post("/", createExpenseRequestController);
+router.post("/employee", createExpenseRequestController);
 router.get("/pending", getPendingRequestsController);
-router.get("/employees/:employeeId", getEmployeeRequestsController); 
+router.get("/employees/:employeeId", getEmployeeRequestsController);
 router.get("/:id", getRequestByIdController);
 router.put("/:id/approve", approveRequestController);
 router.put("/:id/reject", rejectRequestController);
 router.put("/my-requests/:id", updateExpenseRequestController);
 router.delete("/my-requests/:id", deleteExpenseRequestController);
+router.get("/finance/status/:status", getRequestsByStatusController);
 
 export default router;
