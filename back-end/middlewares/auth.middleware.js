ac0df7e6feaadf77a666e4844055e1b7aafc92d6
Luong Minh Nhat <nhatluong.it@gmail.com>
2025-06-18 22:53:38 +0700

    update auth

---
+++ back-end/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import { getEmployeeById } from "../services/authService.service.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No Token Provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+    }
+
+    const employee = await getEmployeeById(decoded.id);
+
+    if (!employee) {
+      return res.status(404).json({ message: "Employee not found" });
+    }
+
+    req.employee = employee;
+
+    next();
+  } catch (error) {
+    console.log("Error in protectRoute middleware: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
